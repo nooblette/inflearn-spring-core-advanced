903dcb5e89acdceccca21a683b1b9349ab832379
nooblette <min9hyuk@gmail.com>
2024-10-13 19:04:42 +0900

    feat : sec 04. 템플릿 메서드 패턴과 콜백 패턴 - 템플릿 메서드 패턴 - 예제3

--- advanced/src/test/java/hello/advanced/trace/template/TemplateMethodTest.java
+++ advanced/src/test/java/hello/advanced/trace/template/TemplateMethodTest.java
@@ -51,4 +51,33 @@ public class TemplateMethodTest {
 		AbstractTemplate template2 = new SubClassLogic2();
 		template2.execute();
 	}
+
+	/**
+	 * 익명 내부 클래스로 템플릿 메서드 패턴 적용
+	 */
+	@Test
+	void templateMethodV2() {
+		// 익명 내부 클래스를 적용하여 template1 인스턴스를 생성하면서 동시에 AbstractTemplate를 상속받는 클래스를 구현할 수 있다.
+		// 쉽게 말해서 객체 인스턴스를 생성하면서 추상 템플릿을 상속받는 클래스를 만드면서 변하는 로직(핵심 비즈니스 로직)을 작성하는 것이다.
+		AbstractTemplate template1 = new AbstractTemplate() {
+			@Override
+			protected void call() {
+				log.info("비즈니스 로직1 실행");
+			}
+		};
+
+		// 익명 내부 클래스의 이름 출력
+		log.info("클래스 이름1={}", template1.getClass());
+		template1.execute();
+
+		AbstractTemplate template2 = new AbstractTemplate() {
+			@Override
+			protected void call() {
+				log.info("비즈니스 로직2 실행");
+			}
+		};
+		// 익명 내부 클래스의 이름 출력
+		log.info("클래스 이름2={}", template2.getClass());
+		template2.execute();
+	}
 }
