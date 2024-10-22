37ea1e8df2e03cc0189996050282647e5c2629b3
nooblette <min9hyuk@gmail.com>
2024-10-23 06:57:52 +0900

    feat : sec 08. 빈 후처리기 - 스프링이 제공하는 빈 후처리기 2

--- proxy/src/main/java/hello/proxy/config/v5_autoproxy/AutoProxyConfig.java
+++ proxy/src/main/java/hello/proxy/config/v5_autoproxy/AutoProxyConfig.java
@@ -1,6 +1,7 @@
 package hello.proxy.config.v5_autoproxy;
 
 import org.springframework.aop.Advisor;
+import org.springframework.aop.aspectj.AspectJExpressionPointcut;
 import org.springframework.aop.support.DefaultPointcutAdvisor;
 import org.springframework.aop.support.NameMatchMethodPointcut;
 import org.springframework.context.annotation.Bean;
@@ -16,11 +17,37 @@ import hello.proxy.trace.logtrace.LogTrace;
 @Import({AppV1Config.class, AppV2Config.class})
 public class AutoProxyConfig {
 
-	@Bean
+	// @Bean
 	public Advisor advisor1(LogTrace logTrace) {
 		// point cut
 		NameMatchMethodPointcut pointcut = new NameMatchMethodPointcut();
 		pointcut.setMappedNames("request*", "order*", "save*");
+
+		// advice 생성
+		LogTraceAdvice logTraceAdvice = new LogTraceAdvice(logTrace);
+
+		// 어드바이저 생성 및 반환 (어드바이저는 하나의 포인트 컷과 하나의 어드바이스를 갖는다.)
+		return new DefaultPointcutAdvisor(pointcut, logTraceAdvice);
+	}
+
+	// @Bean
+	public Advisor advisor2(LogTrace logTrace) {
+		// point cut
+		AspectJExpressionPointcut pointcut = new AspectJExpressionPointcut();
+		pointcut.setExpression("execution(* hello.proxy.app..*(..))");
+
+		// advice 생성
+		LogTraceAdvice logTraceAdvice = new LogTraceAdvice(logTrace);
+
+		// 어드바이저 생성 및 반환 (어드바이저는 하나의 포인트 컷과 하나의 어드바이스를 갖는다.)
+		return new DefaultPointcutAdvisor(pointcut, logTraceAdvice);
+	}
+
+	@Bean
+	public Advisor advisor3(LogTrace logTrace) {
+		// point cut
+		AspectJExpressionPointcut pointcut = new AspectJExpressionPointcut();
+		pointcut.setExpression("execution(* hello.proxy.app..*(..)) && !execution(* hello.proxy.app..noLog(..))");
 
 		// advice 생성
 		LogTraceAdvice logTraceAdvice = new LogTraceAdvice(logTrace);
