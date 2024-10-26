60cdebc264ee2b222aa144bfb85d4a5824c1973b
nooblette <min9hyuk@gmail.com>
2024-10-26 10:54:09 +0900

    feat : sec 14. 스프링 AOP - 실무 주의사항 - 프록시 기술과 한계 - 의존관계 주입

---
+++ aop/src/test/java/hello/aop/proxyvs/ProxyDITest.java
@@ -0,0 +1,30 @@
+package hello.aop.proxyvs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
+
+import hello.aop.order.aop.member.MemberService;
+import hello.aop.order.aop.member.MemberServiceImpl;
+import hello.aop.proxyvs.code.ProxyDIAspect;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+// @SpringBootTest(properties = {"spring.aop.proxy-target-class=false"}) // JDK 동적 프록시 기반 프록시 생성
+@SpringBootTest(properties = {"spring.aop.proxy-target-class=true"}) // CGLIB 기반 프록시 생성
+@Import(ProxyDIAspect.class)
+public class ProxyDITest {
+	@Autowired
+	MemberService memberService;
+
+	@Autowired
+	MemberServiceImpl memberServiceImpl;
+
+	@Test
+	void go() {
+		log.info("memberService class={}", memberService.getClass());
+		log.info("memberServiceImpl class={}", memberServiceImpl.getClass());
+		memberServiceImpl.hello("hello");
+	}
+}
